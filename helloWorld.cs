48c3410162e76431d0594ed534b64a88d4ed957b
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-12 10:49:16 +0530

    change hello world to hello Omkar

--- helloWorld.cs
+++ helloWorld.cs
@@ -10,7 +10,7 @@ namespace helloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Omkar");
             Console.ReadLine();
         }
     }
