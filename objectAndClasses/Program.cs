2eb77b8808a27162e03f9819dd553d564f30867f
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-12 15:21:45 +0530

    Class is created in project

---
+++ objectAndClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objectAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var book =new Book();
+
+        }
+    }
+}
