c1f27a4e6387c3bd587d4561cdfd6b2a0161a10c
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-12 12:44:01 +0530

    Addition syntax Commited

---
+++ Syntax/AdditionSyntax/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionSyntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 57.8621;
+            double y = 35.426;
+            double z = x + y;
+            Console.WriteLine("Addition of x and y is " +z);
+            Console.ReadLine();
+        }
+    }
+}
