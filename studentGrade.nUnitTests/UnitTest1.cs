7925fdb3d98c54fe898d0b5655fffd7c3eb80d90
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-13 09:19:30 +0530

    Test project created fot grade calculator project

---
+++ studentGrade.nUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace studentGrade.nUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
