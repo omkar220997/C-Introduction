69911b9d7c579127b09be2b5bc2c97bf1f2ca263
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-13 08:58:27 +0530

    program of grade calculator class is ready

---
+++ xUnitTest/GradeCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTest
+{
+    public class GradeCalculator
+    {
+        public string GetGradePercentage(int percentage)
+        {
+            if (percentage >= 90 && percentage <= 100)
+                return "A";
+            else if(percentage >= 80 && percentage <90)
+                return "B";
+            else if(percentage >= 70 && percentage < 80)
+                return "C";
+            else if(percentage >= 60 && percentage < 70)
+                return "D";
+            else if(percentage >= 40 && percentage < 60)
+                return "E";
+            else if(percentage >= 0 && percentage < 40)
+                return "Fail";
+            else
+                return "Invalid";
+        }
+    }
+}
